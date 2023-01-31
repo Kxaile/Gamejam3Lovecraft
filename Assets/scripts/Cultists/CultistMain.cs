e2c6bd9cea6ce7722cf9afb2b561075093d24a2e
Kale <kykysmith1642@gmail.com>
2023-01-31 21:13:13 +0000

    Update CultistMain.cs

--- Assets/scripts/Cultists/CultistMain.cs
+++ Assets/scripts/Cultists/CultistMain.cs
@@ -3,7 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CultistMain : MonoBehaviour
-{
+{       
+    /// <summary>      ^                                                                                                ^
+    ///               / \                                                                                              / \     
+    ///              / ! \    THIS SCRIPT IS THE MAIN SCRIPT FOR EACH CULTIST, IT HOLDS THEIR STATS, EVIL OR NOT ETC  / ! \
+    ///             /_____\                                                                                          /_____\     
+    /// </summary>
+
     // Start is called before the first frame update
     void Start()
     {
