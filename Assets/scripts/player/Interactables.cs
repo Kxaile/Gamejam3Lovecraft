711fbf6d91aab3afc347940da092cd8a9ded23a3
ha77y <114589921+ha77y@users.noreply.github.com>
2023-02-09 01:09:10 +0000

    a

--- Assets/scripts/player/Interactables.cs
+++ Assets/scripts/player/Interactables.cs
@@ -7,6 +7,7 @@ public class Interactables : MonoBehaviour
 {
     public GameObject LastHit;
     public LayerMask mask;
+    public bool Highlighted;
 
     // Start is called before the first frame update
     void Start()
@@ -20,23 +21,32 @@ public class Interactables : MonoBehaviour
         RaycastHit hit;
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
+        if (Highlighted && Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            // Interact
+            print("you clicked the " + LastHit.name);
+        }
+
         if (Physics.Raycast(ray, out hit, 25f, mask) && LastHit == null)
         {
-                // highlight object
-                hit.transform.GetChild(0).gameObject.SetActive(true);
+            // highlight object
+            hit.transform.GetChild(0).gameObject.SetActive(true);
 
-                LastHit = hit.transform.gameObject;
-                print("hovering");
+            Highlighted = true;
+
+            LastHit = hit.transform.gameObject;
+            print("hovering");
             return;
         }
         else if(!(Physics.Raycast(ray, out hit,25f,mask)) && LastHit != null)
         {
-                // unhighlight object
-                LastHit.transform.GetChild(0).gameObject.SetActive(false);
-                
+            // unhighlight object
+            LastHit.transform.GetChild(0).gameObject.SetActive(false);
 
-                LastHit = null;
-                print("hovering stopped");
+            Highlighted = false;
+
+            LastHit = null;
+            print("hovering stopped");
         }
     }
 }
