69d61fd0c8a5322983c8fd4d5e9a8b0917e0cbc5
Kale <kykysmith1642@gmail.com>
2023-03-13 16:58:59 +0000

    awdawdwadawd
    awAAAAAAAAAAAAAAAA

--- Assets/scripts/jumpiesD.cs
+++ Assets/scripts/jumpiesD.cs
@@ -4,16 +4,25 @@ using UnityEngine;
 
 public class jumpiesD : MonoBehaviour
 {
+    public float x;
+    public float y;
+    public float z;
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        x = Random.Range(-4, 5);
+        y = Random.Range(-4, 5);
+        z = Random.Range(-4, 5);
+}
 
+    
     // Update is called once per frame
     void Update()
     {
-        if(transform.position.y < 0)
+        //transform.localEulerAngles += new Vector3(Random.Range(-1, 2), Random.Range(-1, 2), Random.Range(-1, 2));
+        transform.rotation *= Quaternion.Euler(x/5, y/1, z/5);
+
+        if (transform.position.y < 0)
 		{
             transform.GetComponent<Rigidbody>().velocity *= -1;
             transform.position = new Vector3(transform.position.x, 0.2f, transform.position.z);
