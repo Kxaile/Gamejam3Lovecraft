73a86b4e4e96decec17133fb6800a4411fd75e41
ha77y <114589921+ha77y@users.noreply.github.com>
2023-04-26 18:32:56 +0100

    settings kicking my ass rn
    it wont display my public sliders as a public variable ???

--- Assets/settings.cs
+++ Assets/settings.cs
@@ -1,23 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class settings : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float Volume;
+    public float brightness;
+    public float fov;
+    [Header("Sliders")]
+    public Slider VolSlider;
+    public Slider BrightSlider;
+    public Slider FovSlider;
+    public struct Controls
     {
-        
+        Input left;
+        Input left_alt;
+        Input Right;
+        Input Right_alt;
+        Input Up;
+        Input Up_alt;
+        Input Down;
+        Input Down_alt;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        VolSlider.value = Volume;
+        BrightSlider.value = brightness;
+        FovSlider.value = fov;
     }
 
-    public void DisplaySettings()
+    public void ChangedValue(float Value, int Indecator)
     {
-
+        switch (Indecator)
+        {
+            case 0: VolSlider.value = Value;
+                break;
+            case 1: BrightSlider.value = Value;
+                break;
+            case 2: FovSlider.value = Value;
+                break;
+        }
     }
+
 }
+
